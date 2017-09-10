2400de29d3ed4003cfb86e149f0f9dc3824c79d5
Amh <abaams78@gmail.com>
2017-09-10 16:25:19 -0500

    lab final

--- Lesson5/simpleApp/routes/users.js
+++ Lesson5/simpleApp/routes/users.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var fetch = require('node-fetch');
 const Rx = require('@reactivex/rxjs');
 
-
 //using Promise
 /*router.get('/', function(req, res, next){
     fetch('http://jsonplaceholder.typicode.com/users/')
@@ -12,26 +11,34 @@ const Rx = require('@reactivex/rxjs');
                                              users: data}));
 });*/
 //Observable
-/*router.get('/', function(req,res, next){
+router.get('/', function(req,res, next){
     Rx.Observable.fromPromise(fetch('http://jsonplaceholder.typicode.com/users/'))
-                 .map(users => users.json())
-                 .subscribe(users => {
-                   console.log(users);
-                   res.render('users', {title:'Users data', users: users})
-                  });
-});*/
+                 .subscribe(users =>  res.render('users', {title:'Users data', users: data}));
+});
 //Asnyc and wait
+/*var getJson = new Promise((resolve, reject) => {
+                    fetch('http://jsonplaceholder.typicode.com/users/')
+                        .then(res => {
+                        if (res.ok) {
+                            return res.json();
+                        }
+                        else {
+                            reject("Data not found");
+                        }
+                        })
+                        .then(data => resolve(data));
+                    });
 async function asnycAwait(){
             try {
-                let results = await fetch('http://jsonplaceholder.typicode.com/users/');                             
+                let users = await getJson;
+                return users;                          
             } catch (error) {
               console.log(error);
             }
         }
         
 router.get('/', function(req,res, next){
-    res.render('users', {title:'Users data', users: asnycAwait()});
-});
-
+    asnycAwait().then(data => res.render('users', {title:'Users data', users: data}))
+});*/
 
 module.exports = router;
